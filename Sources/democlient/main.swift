3adb1186f8a13a642623ec862e07188358b92b65
Nevyn Bengtsson <hello@nevyn.dev>
2025-03-13 14:19:59 +0100

    democlient creates an entity on setup

--- Sources/democlient/main.swift
+++ Sources/democlient/main.swift
@@ -6,23 +6,47 @@
 //
 
 import Foundation
+import Combine
 import allonet2
+
+@MainActor
+class DemoApp
+{
+    let client: AlloClient
+    init(connectingTo url: URL)
+    {
+        print("Connecting to alloverse swift place ", url)
+
+        self.client = AlloClient(url: url, avatarDescription: [
+        ])
+        
+        Task { [weak self] in
+            guard let vals = self?.client.$isAnnounced.values else { return }
+            for await announced in vals where announced == true {
+                guard self != nil else { break }
+                do {
+                    try await self?.setup()
+                } catch(let e) {
+                    print("Failed setup: \(e)")
+                    exit(1)
+                }
+            }
+        }
+
+        client.stayConnected()
+    }
+    
+    func setup() async throws
+    {
+        print("Demo app connected, setting up...")
+        let eid = try await client.createEntity(with: [])
+        print("Whee fresh eid: \(eid)")
+    }
+}
 
 
 let url = URL(string: CommandLine.arguments[1])!
-
-print("Connecting to alloverse swift place ", url)
-
-let client = AlloClient(url: url, avatarDescription: [
-
-])
-
-client.stayConnected()
-
-
-// once connected, send announce
-
-// when received response, hand over to worldclient to receive world state
+let app = DemoApp(connectingTo: url)
 
 func park() async -> Never {
     await withUnsafeContinuation { _ in }
